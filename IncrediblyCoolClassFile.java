b5ad694a8e2c8efc3627b711e56b40714c07a024
jblanchard <josh.blanchard@infernored.com>
2013-10-02 15:45:03 -0400

    This is a completely dangerous change, we're going to want to revert it

--- IncrediblyCoolClassFile.java
+++ IncrediblyCoolClassFile.java
@@ -1,3 +1,5 @@
 //Hey check it out, there's some stuff here.
 
 //Hey look, even more stuff, we should totally commit and push this stuff
+
+//I'm not feeling very good about this change, but I'm going to make it anyway.
