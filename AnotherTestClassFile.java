cbf37b358d261884211e6e029acf1f050ff24a0b
joshblanchard <josh.blanchard@infernored.com>
2013-11-04 13:48:57 -0500

    test

--- AnotherTestClassFile.java
+++ AnotherTestClassFile.java
@@ -7,24 +7,12 @@ Line 2
 Line 3
 
 Line 4
-<<<<<<< .mine
 
-=======
 Josh's Change
->>>>>>> .theirs
+
 Line 5
-<<<<<<< .mine
-
-=======
-
->>>>>>> .theirs
 Mark's thrird change
-<<<<<<< .mine
-
-
-=======
 
 Mark's third change
->>>>>>> .theirs
 Mark's 4th Change
 Mark's 5th Change
