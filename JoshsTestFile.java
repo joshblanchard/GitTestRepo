e066c7cc4d41d3e090f8c621bb56b16d064e6bc9
joshblanchard <josh.blanchard@infernored.com>
2014-02-12 12:39:51 -0500

    fixed merge conflict

--- JoshsTestFile.java
+++ JoshsTestFile.java
@@ -4,15 +4,13 @@
 // Line J2
 // Line J3// Line M1
 // Line M2
-<<<<<<< HEAD
+
 // Line M3
 // Line M3
 // Line M3
 
 // Line M6
-//Line J4
+// Line J4
 // Line M7
-//Line J5
-=======
+// Line J5
 // Line M3
->>>>>>> parent of ee85bb5... Josh's commit
