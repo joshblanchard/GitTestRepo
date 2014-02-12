605d81f8c637bb7379db67130310e495beee1ec4
joshblanchard <josh.blanchard@infernored.com>
2014-02-12 12:34:12 -0500

    Revert "Josh's commit"
    This reverts commit ee85bb56d069901546155e179c6ff249dd2ea0eb.
    
    Conflicts:
    	JoshsTestFile.java

--- JoshsTestFile.java
+++ JoshsTestFile.java
@@ -2,9 +2,9 @@
 
 // Line J1
 // Line J2
-// Line J3
-// Line M1
+// Line J3// Line M1
 // Line M2
+<<<<<<< HEAD
 // Line M3
 // Line M3
 // Line M3
@@ -13,3 +13,6 @@
 //Line J4
 // Line M7
 //Line J5
+=======
+// Line M3
+>>>>>>> parent of ee85bb5... Josh's commit
